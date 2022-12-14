23b3bb429325acedd113947d7c2ec1bfdeb83b80
pachanda <oleggud52@gmail.com>
2022-12-14 13:24:27 +0200

    1

--- server/src/database/index.js
+++ server/src/database/index.js
@@ -1,15 +1,9 @@
 const { Sequelize } = require("sequelize")
-
 const Todo = require("./Todo")
 
-const sequelize = new Sequelize({
-  database: "privet",
-  username: "root",
-  password: "Ab7Cug84",
-  host: "localhost",
-  port: 3306,
-  dialect: "mysql"
-})
+const config = require("./config")
+
+const sequelize = new Sequelize(config)
 
 Todo.init(sequelize)
 
